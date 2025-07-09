42022082e2511b48c9ceb5d1809a865572b3e51d
Andrés Ignacio Torres <dev@aitorres.com>
2025-07-08 23:45:00 -0700

    style: formatting tweaks to infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,18 +1,18 @@
 // Resource group to host all Azure resources for the project
 resource "azurerm_resource_group" "resource_group" {
-    name = var.azure_resource_group_name
-    location = var.azure_location
+  name     = var.azure_resource_group_name
+  location = var.azure_location
 }
 
 // Container registry for storing Docker images
 resource "azurerm_container_registry" "container_registry" {
-    name                = "csca5028finalprojectacr"
-    location            = azurerm_resource_group.resource_group.location
-    resource_group_name = azurerm_resource_group.resource_group.name
-    sku                 = "Basic"
-    admin_enabled       = true
+  name                = "csca5028finalprojectacr"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  sku                 = "Basic"
+  admin_enabled       = true
 
-    tags = {
-        environment = "production"
-    }
+  tags = {
+    environment = "production"
+  }
 }
--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,22 +1,22 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "~> 4.35.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.35.0"
     }
+  }
 
-    backend "azurerm" {
-        resource_group_name  = "final-project-state-rg"
-        storage_account_name = "terraformfinalprojstate"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
+  backend "azurerm" {
+    resource_group_name  = "final-project-state-rg"
+    storage_account_name = "terraformfinalprojstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
 
   subscription_id = var.azure_subscription_id
-    tenant_id       = var.azure_tenant_id
+  tenant_id       = var.azure_tenant_id
 }
