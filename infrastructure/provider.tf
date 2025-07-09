b6fb6e203dd37dd44468e5e532616639c45f7459
Andrés Ignacio Torres <dev@aitorres.com>
2025-07-08 22:56:14 -0700

    fix: use az login with credentials

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,5 +1,19 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "~> 3.0"
+        }
+    }
+
+    backend "azurerm" {
+        resource_group_name  = "final-project-state-rg"
+        storage_account_name = "terraformfinalprojstate"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
 provider "azurerm" {
   features {}
-
-  use_oidc = true
 }
